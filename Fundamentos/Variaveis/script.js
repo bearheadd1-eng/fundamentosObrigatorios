269e00203d2936ded7e3935e00db7a68e7221fdf
d1Brenox <breno.x377@gmail.com>
2026-05-14 10:46:12 -0300

    Create script.js

---
+++ Fundamentos/Variaveis/script.js
@@ -0,0 +1,43 @@
+//Oque é uma variavel?
+let nome = "Breno";
+let idade = 25;
+let altura = 1.75;
+
+// Esctrutura de uma variavel
+nomeDaVariavel = valor 
+let cidade = "São Paulo";
+// let é a palavra reservada para criar uma variavel
+// nomeDaVariavel é o nome da variavel, cidade = nome da variavel, "São Paulo" é o valor da variavel
+
+// Tipos de dados nas variaveis
+let nome = "Breno"; // String
+//strings ficam entre aspas "" ou '' ou `` (template literals)
+
+let idade = 25; // Number
+let altura = 1.75; // Number
+// Números podem ser inteiros ou decimais
+
+let isStudent = true; // Boolean
+// Booleanos só podem ser true ou false (verdadeiro ou falso)
+
+//Criando variaveis em JavaScript
+//Usando let (podemos mudar o valor da variavel depois de criada)
+let idade = 20;
+idade = 21// Podemos criar uma variavel e depois atribuir um valor a ela
+
+//Usando const (não podemos mudar o valor da variavel depois de criada)
+const pi = 3.14;
+//pi = 3.14159; // Isso vai dar um erro, pois pi é uma constante e não pode ser reatribuida
+
+//Usando var (antiga forma de criar variaveis, não recomendado)
+var nome = "Breno";
+// var tem escopo global ou de função, o que pode causar problemas em projetos maiores, por isso é recomendado usar let ou const
+
+//Usando variaveis juntas
+let nome = 'Breno';
+let idade = 25;
+let cidade = 'São Paulo';
+//saida
+console.log(nome); // Imprime "Breno"
+console.log(idade); // Imprime 25
+console.log(cidade); // Imprime "São Paulo"

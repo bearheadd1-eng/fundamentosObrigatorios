9b1ca87ee40e2d2af14e09fed96b2486cfb67249
d1Brenox <breno.x377@gmail.com>
2026-05-29 08:08:07 -0300

    GitHub

---
+++ GitHub/script.js
@@ -0,0 +1,105 @@
+// Oque é GitHub?
+// GitHub é uma plataforma de hospedagem de código-fonte e controle de versão usando o sistema Git. Ele permite que os desenvolvedores colaborem em projetos, compartilhem código e gerenciem mudanças de forma eficiente. O GitHub é amplamente utilizado para desenvolvimento de software, mas também pode ser usado para armazenar e compartilhar outros tipos de arquivos.
+
+// Git =  Ferramenta que salva verões do código
+// GitHub = Plataforma que hospeda o código e permite colaboração
+
+// O que é o Git?
+// O Git é um sistema de controle de versão distribuído que permite aos desenvolvedores rastrear mudanças no código-fonte ao longo do tempo. Ele facilita a colaboração entre equipes, permitindo que várias pessoas trabalhem no mesmo projeto simultaneamente, sem conflitos. O Git armazena o histórico completo do projeto, permitindo que os desenvolvedores revertam para versões anteriores, criem ramificações (branches) para desenvolvimento paralelo e mesclem mudanças de forma eficiente.
+
+// Exemplo
+// Você criou um site
+// Depois:
+// adicionou botão
+// mudou cor
+// corrigiu erro
+// apagou algo sem querer
+
+// O git permite:
+// voltar versões antigas
+// ver o que mudou
+// saber quem mudou
+// trabalhar em equipe sem bagunça
+
+// Como o GitHub Funciona?
+// O fluxo básico é assim:
+// seu  computador -> git -> GitHub
+
+// você programa no computador.
+// Depois usa o Git para:
+// Salvar alterações
+// Cria Versões
+// Enviar para o GitHub
+
+// No GitHub:
+// outras pessoas podem ver seu código
+// colaborar
+// baixar
+// comentar
+// sugerir mudanças
+
+//Conceitos Mais importantes do GitHub:
+// 1. Repositório (Repository): É onde o código-fonte do projeto é armazenado. Pode ser público (qualquer pessoa pode ver) ou privado (apenas pessoas autorizadas podem acessar).
+
+// É a pasta do projeto no GitHub, onde ficam os arquivos, código, documentação, etc.
+
+//Exemplo:
+//meu-seite/
+
+// No GitHub isso vira um "Repositório".
+
+// Pode conter:
+// HTML
+// CSS
+// JavaScript
+// Imagens
+// Documentação
+// Qual quer outro arquivo
+
+// 2. Commit: É uma "foto" do estado atual do código. Cada commit tem uma mensagem que descreve as mudanças feitas. Ele permite que você salve o progresso do seu trabalho e volte a versões anteriores se necessário.
+
+// Exemplo:
+// "Adicionado botão de login"
+
+// Cada commit guarda:
+// O que mudou
+// Quem mudou
+// Quando mudou
+
+// É como um chekpoint no jogo, onde você pode voltar se algo der errado.
+
+// 3. Push: É o processo de enviar suas mudanças locais (commits) para o repositório remoto no GitHub. Isso torna suas alterações visíveis para outras pessoas que têm acesso ao repositório.
+
+// Push = Enviar suas mudanças para o GitHub
+// git push
+
+// 4. Pull Request: É uma solicitação para mesclar suas mudanças (commits) em um branch principal do projeto. Ele permite que outros revisem seu código, façam comentários e aprovem as mudanças antes de serem integradas ao projeto principal.
+// Pull = baixar atualizações do GitHub para o seu computador
+// git pull
+
+// 5. Clone: É o processo de criar uma cópia local de um repositório remoto do GitHub. Isso permite que você trabalhe no código em seu próprio ambiente de desenvolvimento e faça alterações antes de enviar suas mudanças de volta para o repositório remoto.
+// Clone = Baixar uma cópia do repositório para o seu computador
+// git clone
+
+// 6. Branch: É uma ramificação do código principal (geralmente chamada de "main" ou "master") onde você pode desenvolver novas funcionalidades ou corrigir bugs sem afetar o código principal. Depois de concluir as mudanças, você pode mesclar (merge) a branch de volta ao código principal.
+// Branch = Uma "Linha paralela" do projeto onde você pode trabalhar sem afetar o código principal.
+// Exemplo:
+// Projeto Principal
+//     |
+//     |-- nova-funcionalidade (Branch)
+//     |-- correção-de-bug (Branch)
+//     |-- tela-de-login (Branch)
+
+// Você criou branches para testar coisas sem quebrar o projeto principal. Depois de terminar, você pode mesclar (merge) a branch de volta ao código principal.
+
+// 7. Pull Request: É uma solicitação para mesclar suas mudanças (commits) em um branch principal do projeto. Ele permite que outros revisem seu código, façam comentários e aprovem as mudanças antes de serem integradas ao projeto principal.
+// Pull Request = Solicitação para mesclar suas mudanças em um branch principal do projeto.
+// Você pede para juntar sua branch ao projeto principal.
+// A euqipe revisa seu código, comenta, sugere mudanças e aprova antes de juntar ao projeto principal.
+
+// O que dá pra fazer no GitHub?
+// Hospedar projéto 
+// Trabalhar em equipe
+// Criar Portfólio
+// Participar de Open Source
+
